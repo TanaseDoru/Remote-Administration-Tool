bf02122f3f8b273a8a415e65fa6c0b5fb90e4521
Vladuceanu Tudor <tudorvladuceanu@gmail.com>
2025-01-11 21:00:34 +0200

    deamon

--- client.c
+++ client.c
@@ -16,6 +16,47 @@ clientInfo_t clientData;
 
 int main()
 {
+    pid_t pid, sid;
+
+    pid = fork();
+
+    if (pid < 0) {
+        // Fork failed
+        perror("Fork failed");
+        exit(1);
+    }
+
+    
+    if (pid > 0) {
+        printf("Parent process exiting, child will be adopted by init\n");
+        exit(0);
+    }
+
+    
+    sid = setsid();
+    if (sid < 0) {
+        perror("Failed to create a new session");
+        exit(1);
+    }
+
+    
+    if (chdir("/") < 0) {
+        perror("Failed to change working directory to /");
+        exit(1);
+    }
+
+    
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    open("/dev/null", O_RDONLY);  
+    open("/dev/null", O_WRONLY);  
+    open("/dev/null", O_WRONLY);  
+
+    
+    printf("This process is now detached and running as a daemon.\n");
+
     int sock;
     connectionInitialize(&sock);
     clientData.serverSocket = sock;
